e3625039ab27a3a3203f3baafc6aaed3a5104202
AntoCreed777 <abenavides@desem.cl>
2023-06-21 23:00:59 -0400

    Este si

--- Envio_data_Server3/Envio_data_Server3.ino
+++ Envio_data_Server3/Envio_data_Server3.ino
@@ -1,30 +1,18 @@
 #include <ESP8266WiFi.h>
-#include <Ethernet.h>//libreria ethernet
-#include <SPI.h>//libreria ethernet
 #include <WiFiClient.h>
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "dbenavid";
-const char* password = "Dial-7681810";
+const char* ssid = "S91 Pro";
+const char* password = "ant.WIFI";
 
 String serverName = "http://pepa.desem.cl/antonio/update-sensor.php";
-
-int hum, luz;//constantes para humedad
-int sensorhumPIN=A5;
-int sensorluzPIN=A4;
-
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xEE}; // Direccion MAC
-byte ip[] = { 192,168,1,109 }; // Direccion IP del Arduino
-byte server[] = { 192,168,1,91 }; // Direccion IP del servidor192,168,0,14
-
-DHT dht(DHTPIN, DHTTYPE);       //Definimos objeto tipo dht
-EthernetClient cliente;         //objeto del ethernet
 
 void setup() {
   Serial.begin(9600);           //Inicializamos comunicación serie
   WiFi.begin(ssid, password);
- 
+  pinMode(D6,OUTPUT);
+  pinMode(D7,OUTPUT);
+  pinMode(A0,INPUT);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -32,17 +20,24 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  
-  Ethernet.begin(mac, ip);      // Inicializamos el Ethernet Shield
 }
 
 void loop() {
   Serial.println("Leyendo datos");
   Serial.println("espere...");
   
-  //codigo para la temperatura
-  int hum = analogRead(sensorhumPIN);// Leemos la humedad
-  int luz = analogRead(sensorluzPIN);// Leemos la luz
+  digitalWrite(D6,HIGH);
+  delay(500);
+  int hum = analogRead(A0);// Leemos la humedad
+  delay(500);
+  digitalWrite(D6,LOW);
+  /////////////////////////////////////////////////////
+  digitalWrite(D7,HIGH);
+  delay(500);
+  int luz = analogRead(A0);// Leemos la luz
+  delay(500);
+  digitalWrite(D7,LOW);
+  
   luz=constrain(luz,0,100);
   luz=map(luz,0,100,100,0);
   
@@ -56,8 +51,8 @@ void loop() {
 
   WiFiClient client;
   HTTPClient http;
-  String strhum = conversion(hum);
-  String strluz= conversion(luz);
+  String strhum = String(hum);
+  String strluz= String(luz);
   String serverPath = serverName+"?hum="+strhum+"&luz="+strluz;
   
   http.begin(client, serverPath.c_str());
@@ -84,9 +79,3 @@ void loop() {
   delay(5000);
   
 }
-String conversion(int valor){
-  String strvalor;
-  strvalor += valor;
-  return(strvalor);
-    
-    }
